d123bbf680a319134f64bd2863fcfad89130bd00
enrique <erivasf@fpcoruna.afundacion.org>
2022-10-04 12:59:57 +0200

    Tarea 6 php completada

--- sprint1apache/www/site3/conversor2.php
+++ sprint1apache/www/site3/conversor2.php
@@ -9,20 +9,25 @@
 					$v_pulgadas = $_POST["fcantidad"];
 					$v_metros = $v_pulgadas * 0.0254;
 					echo $v_pulgadas."pulgada(s) = ".$v_metros." metro(s)";
-				}else{
+				}elseif ($_POST["funidad"] == "yarda"){
+				$v_yarda = $_POST ["fcantidad"];
+				$v_metros = $v_yarda * 0.9144;
+					echo $v_yarda."yarda(s) = ".$v_metros."metro(s) ";
+				} else{
 					echo "Unidad no soportada";
 				}
 			}
 		?>
 		</p>
-		<p>Realiza una nueva conversación:</p>
+		<p>Realiza una nueva conversión:</p>
 		<form action="/conversor2.php" method="post">
 			<label for="cantidad_input">Cantidad:</label><br>
-			<input type="text" id="cantidad_input" name="fcantidad"><br>
-			
+			<input type="text" id="cantidad_input" name="fcantidad"><br>		
 			<input type="radio" id="pulgada_input" name="funidad" value="pulgada">
 			<label for="pulgada_input">Pulgada(s)</label><br>
-			<input type="radio" id="otro_input" name="funidad" value="otro">
+			<input type="radio" id="yarda_input" name="funidad" value="yarda">
+			<label for="yarda_input" >Yarda(s) </label><br>
+			<input type="radio" id="otro_input"name="funidad" value="otro">
 			<label for="otro_input">Otro</label><br>
 
 			<input type="submit" value="Convertir">
