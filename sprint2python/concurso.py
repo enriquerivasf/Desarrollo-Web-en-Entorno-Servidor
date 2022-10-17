c92164e0e25b8cff50f42c8e81f6abfa69107517
enriquerivasf <erivasf@fpcoruna.afundacion.org>
2022-10-17 21:55:14 +0200

    Tarea 1 completada

--- sprint2python/concurso.py
+++ sprint2python/concurso.py
@@ -1,1 +1,19 @@
-kdk
+print("¡Adivina!")
+print("Cual es mi color favorito?")
+print("a) Verde ")
+print("b) Rojo")
+print("c) Morado")
+
+respuesta =""
+
+respuesta = input("Elige una de las opciones(a,b,c): ")
+
+while(respuesta !="a" and respuesta !="b" and respuesta !="c" ):
+    respuesta = input ("Elige una de las opciones(a,b,c): ")
+    
+if respuesta =="c":
+    print("Acertaste")
+else:
+    print("Fallaste")
+    
+        
