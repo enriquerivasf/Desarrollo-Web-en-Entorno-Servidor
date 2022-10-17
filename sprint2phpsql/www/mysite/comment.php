1dea37221f5a2e11c0fd70c3094e082142adff5c
EnriqueRivas <92723398+enriquerivasf@users.noreply.github.com>
2022-10-17 13:45:27 +0200

    Update comment.php

--- sprint2phpsql/www/mysite/comment.php
+++ sprint2phpsql/www/mysite/comment.php
@@ -15,7 +15,7 @@
 			echo mysqli_insert_id($db);
 			echo " añadido</p>";
 
-			echo "<a href='/detail.php?id=".$pelicula_id."'>Volver</a>";
+			echo "<a href='/detail.php?pelicula_id=".$pelicula_id."'>Volver</a>";
 			mysqli_close($db);
 		?>
 	</body>
