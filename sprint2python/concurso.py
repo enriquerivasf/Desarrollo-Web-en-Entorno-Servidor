fc271776accd53494fd84f26416d0eda158a2956
enriquerivasf <erivasf@fpcoruna.afundacion.org>
2022-10-17 22:45:50 +0200

    Tarea 2 completada

--- sprint2python/concurso.py
+++ sprint2python/concurso.py
@@ -1,8 +1,54 @@
+print(" ")
+print(" ")
 print("¡Adivina!")
 print("Cual es mi color favorito?")
 print("a) Verde ")
 print("b) Rojo")
 print("c) Morado")
+
+respuesta =""
+cont=0
+
+respuesta = input("Elige una de las opciones(a,b,c): ")
+
+while(respuesta !="a" and respuesta !="b" and respuesta !="c" ):
+    respuesta = input ("Elige una de las opciones(a,b,c): ")
+    
+if respuesta =="c":
+    print("Acertaste")
+    cont=cont + 10
+else:
+    print("Fallaste")
+    cont=cont - 5
+    print(" ")
+    print(" ")
+print("¡Adivina!")
+print("Cual es mi serie favorita?")
+print("a) La casa del dragón ")
+print("b) El Señor de los Anillos: los Anillos de Poder")
+print("c) Rick y Morty")
+
+respuesta =""
+
+respuesta = input("Elige una de las opciones(a,b,c): ")
+
+while(respuesta !="a" and respuesta !="b" and respuesta !="c" ):
+    respuesta = input ("Elige una de las opciones(a,b,c): ")
+    
+if respuesta =="a":
+    print("Acertaste")
+    cont=cont + 10
+else:
+    print("Fallaste")
+    cont=cont - 5
+    
+print(" ")
+print(" ")
+print("¡Adivina!")
+print("Cuanto es 5+2?")
+print("a) 5 ")
+print("b) 2")
+print("c) 7")
 
 respuesta =""
 
@@ -13,7 +59,11 @@ while(respuesta !="a" and respuesta !="b" and respuesta !="c" ):
     
 if respuesta =="c":
     print("Acertaste")
+    cont=cont + 10
 else:
-    print("Fallaste")
+    print("Fallaste") 
+    cont=cont - 5   
     
-        
+print("Has conseguido "+ str(cont) + " puntos")
+print(" ")
+print(" ")
