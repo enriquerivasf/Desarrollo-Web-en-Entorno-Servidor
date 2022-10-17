c259d2818551b149d9db4f46883350aa187c50e9
EnriqueRivas <92723398+enriquerivasf@users.noreply.github.com>
2022-10-17 13:49:29 +0200

    Update detail.php

--- sprint2phpsql/www/mysite/detail.php
+++ sprint2phpsql/www/mysite/detail.php
@@ -8,7 +8,7 @@
 				die('No se ha especificado una película');
 			}
 			$pelicula_id =$_GET['pelicula_id'];
-			$query ='SELECT * FROM tPeliculas WHERE id='.$pelicula_id;
+			$query ='SELECT * FROM tPeliculas WHERE pelicula_id='.$pelicula_id;
 			$result= mysqli_query($db, $query) or die('Query error');
 			$only_row = mysqli_fetch_array($result);
 			echo '<h1>'.$only_row['id'].'</h1>';
@@ -21,7 +21,7 @@
 		<h3>Comentarios:</h3>
 		<ul>
 			<?php
-				$query2 = 'SELECT * FROM tComentarios WHERE id='.$pelicula_id;
+				$query2 = 'SELECT * FROM tComentarios WHERE pelicula_id='.$pelicula_id;
 				$result2= mysqli_query($db, $query2) or die('Query error');
 				while ($row = mysqli_fetch_array($result2)) {
 					echo '<li>'.$row['comentario']. " ".$row['fecha'].'</li>';
