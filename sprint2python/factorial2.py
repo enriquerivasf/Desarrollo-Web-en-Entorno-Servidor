b63b7e9d50714afd461161ce5f7b8e71a2da4c47
EnriqueRivas <92723398+enriquerivasf@users.noreply.github.com>
2022-10-18 14:35:50 +0200

    Revert "Tarea 4 completada"
    This reverts commit 24fe6f7f64d71bdb783e48a8b6762fe305d9638e.

--- sprint2python/factorial2.py
+++
@@ -1,9 +0,0 @@
-def func_factorial2(numero):
-    if numero==0 or numero==1:
-            numero=1
-    else: 
-        factorial = 1
-        while(numero > 1): 
-            factorial *= numero 
-            numero -= 1
-        return factorial 
--- sprint2python/principal.py
+++ sprint2python/principal.py
@@ -1,17 +1,5 @@
 from factorial import func_factorial
-from factorial2 import func_factorial2
+numero=input("Introduce un número para calcular el factorial: ")
+numero=int(numero)
 
-print("a) Calcular el factorial con recursividad ")
-print("b) Calcular el factorial sin recursividad")
-opcion=input("Elige una opción: ")
-
-
-
-
-if(opcion== "a"):
-    numero=int(input("Introduce un número para calcular el factorial: "))
-    print("El factorial del numero "+ str(numero) + " es "+ str(func_factorial(numero)))
-if(opcion== "b"):
-    
-    numero=int(input("Introduce un número para calcular el factorial: "))
-    print("El factorial del numero "+ str(numero) + " es "+ str(func_factorial2(numero)))
+print("El factorial del numero "+ str(numero) + " es "+ str(func_factorial(numero)))
