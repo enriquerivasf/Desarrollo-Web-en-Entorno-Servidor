c5e6567a73cc2454c8ec1fac08e8bc2fe816ddac
EnriqueRivas <92723398+enriquerivasf@users.noreply.github.com>
2022-10-18 14:54:00 +0200

    Tarea 6 completada

--- sprint2python/principal.py
+++ sprint2python/principal.py
@@ -1,3 +1,4 @@
+import time
 from factorial import func_factorial
 from factorial2 import func_factorial2
 
@@ -9,9 +10,19 @@ opcion=input("Elige una opción: ")
 
 
 if(opcion== "a"):
+    start_time = time.time()
     numero=int(input("Introduce un número para calcular el factorial: "))
     print("El factorial del numero "+ str(numero) + " es "+ str(func_factorial(numero)))
-if(opcion== "b"):
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print('El tiempo de ejecución ha sido :' + str(elapsed_time) + "s")
+   
     
+if(opcion== "b"):
+   
+    start_time = time.time()
     numero=int(input("Introduce un número para calcular el factorial: "))
     print("El factorial del numero "+ str(numero) + " es "+ str(func_factorial2(numero)))
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print('El tiempo de ejecución ha sido :' + str(elapsed_time) + "s")
