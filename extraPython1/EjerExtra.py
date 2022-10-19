a8bb355a6d1af341f11f587c600677cdbaeebf86
enriquerivasf <erivasf@fpcoruna.afundacion.org>
2022-10-19 17:34:24 +0200

    Ejercicio Extra Completado

--- extraPython1/EjerExtra.py
+++ extraPython1/EjerExtra.py
@@ -6,54 +6,64 @@ cont = 0
 puntuacionJugador = 0
 puntuacionMaquina = 0
 while cont < 5:
-    opcion = input("Que vas a sacar??\n 1)piedra\n papel=2, tijera=3")
-    while (opcion != 1 and opcion != 2 and opcion != 3):
-        opcionJugador = input("Que vas a sacar??\n 1)piedra\n 2)papel=2\n 3)tijera=3\n")
+    opcionJugador= int(input("Elige una opcion (1,2,3) \n 1)piedra \n 2)papel \n 3)tijera \n  "))
+    print(" ")
+    if(opcionJugador==1 or opcionJugador==2 or opcionJugador==3):
 
-    opcionMaquina = random.randint(1, 3)
+        opcionMaquina = random.randint(1, 3)
 
-    if (opcionJugador == 1 and opcionMaquina == 1):
-        print("Empate")
-        cont = cont + 1
+        if (opcionJugador==1 and opcionMaquina==1):
+            print("Empate")
+            cont = cont + 1
 
-    elif (opcionJugador == 1 and opcionMaquina == 2):
-        print("Has perdido, piedra pierde contra papel ")
-        cont = cont + 1
-        puntuacionMaquina = puntuacionMaquina + 1
+        elif (opcionJugador==1 and opcionMaquina==2):
+            print("Has perdido, piedra pierde contra papel ")
+            cont = cont + 1
+            puntuacionMaquina = puntuacionMaquina + 1
 
-    elif (opcionJugador == 1 and opcionMaquina == 3):
-        print("Has ganado, piedra gana a tijera")
-        cont = cont + 1
-        puntuacionJugador = puntuacionJugador + 1
+        elif (opcionJugador == 1 and opcionMaquina == 3):
+            print("Has ganado, piedra gana a tijera")
+            cont = cont + 1
+            puntuacionJugador = puntuacionJugador + 1
 
-    elif(opcionJugador == 2 and opcionMaquina == 1):
-        print("Has ganado, papel gana a piedra")
-        cont = cont + 1
-        puntuacionJugador = puntuacionJugador + 1
+        elif(opcionJugador == 2 and opcionMaquina == 1):
+            print("Has ganado, papel gana a piedra")
+            cont = cont + 1
+            puntuacionJugador = puntuacionJugador + 1
 
-    elif(opcionJugador == 2 and opcionMaquina == 2):
-        print("Empate")
-        cont = cont + 1
+        elif(opcionJugador == 2 and opcionMaquina == 2):
+            print("Empate")
+            cont = cont + 1
 
 
-    elif (opcionJugador == 2 and opcionMaquina == 3):
-        print("Has perdido, papel pierde contra tijera")
-        cont = cont + 1
-        puntuacionMaquina = puntuacionMaquina + 1
+        elif (opcionJugador == 2 and opcionMaquina == 3):
+            print("Has perdido, papel pierde contra tijera")
+            cont = cont + 1
+            puntuacionMaquina = puntuacionMaquina + 1
 
-    elif(opcionJugador == 3 and opcionMaquina == 1):
-        print("Has perdido, tijera pierde contra piedra")
-        cont = cont + 1
-        puntuacionMaquina = puntuacionMaquina + 1
+        elif(opcionJugador == 3 and opcionMaquina == 1):
+            print("Has perdido, tijera pierde contra piedra")
+            cont = cont + 1
+            puntuacionMaquina = puntuacionMaquina + 1
+        
+        elif(opcionJugador == 3 and opcionMaquina == 2):
+            print("Has ganado, tijera gana contra papel")
+            cont = cont + 1
+            puntuacionJugador = puntuacionJugador + 1
+        
+        elif(opcionJugador == 3 and opcionMaquina == 3):
+            print("Empate")
+            cont = cont + 1
+        print(" ")
+        print("Jugador: " + str(puntuacionJugador) + " | " + "Maquina: "+ str(puntuacionMaquina))   
+        print(" ")
+    else :
+        print("No has escogido una opción válida")
+        print(" ")
+if(puntuacionJugador>puntuacionMaquina):
+    print(" ")
+    print("Felicidades, has ganado")
+else:
+    print(" ")
+    print("Lo siento, has perdido")
     
-    elif(opcionJugador == 3 and opcionMaquina == 2):
-        print("Has ganado, tijera gana contra papel")
-        cont = cont + 1
-        puntuacionJugador = puntuacionJugador + 1
-    
-    elif(opcionJugador == 3 and opcionMaquina == 3):
-        print("Empate")
-        cont = cont + 1
-
-    print("Jugador: "+ puntuacionJugador + " | "+ puntuacionMaquina)   
-
