e744bcd6dcd7396c58c5bd94dd69a6e476bb7995
enrique <erivasf@fpcoruna.afundacion.org>
2022-10-26 14:13:10 +0200

    Ejercicio 8 completado con exito

--- sprint3sesiones/www/mysite/cambiarContrasena.php
+++ sprint3sesiones/www/mysite/cambiarContrasena.php
@@ -1,14 +1,16 @@
 <?php
     $db = mysqli_connect('localhost', 'root', '1234', 'mysitedb') or die('Fail');
-    $contrasenaIntroducida= $_POST['f_password'];
+    
     session_start();
-
+$contrasenaIntroducida= $_POST['contrasena'];
+	echo $_SESSION['user_id'];
+	echo "$contrasenaIntroducida";
     if (empty($_SESSION['user_id'])) {
         echo "Error: es necesario tener sesión iniciada";
     } else {
        
-        $query1 = "UPDATE tUsuarios SET contraseña='".$contrasenaIntroducida."' WHERE id=".$_SESSION['user_id'].";";
-        $result1 = mysqli_query($db, $query1 or die('Error al actualizar la contraseña');
+        $query1 = "UPDATE tUsuarios SET contraseña='".$contrasenaIntroducida."' WHERE id=".$_SESSION['user_id'];
+        $result1 = mysqli_query($db, $query1) or die('Error al actualizar la contraseña');
 
       
         if ($result1) {
