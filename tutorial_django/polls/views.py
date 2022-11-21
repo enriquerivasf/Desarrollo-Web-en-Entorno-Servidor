30456c3abbdc08487c239581de2ddcf19f1d6046
enrique <erivasf@fpcoruna.afundacion.org>
2022-11-21 23:33:49 +0100

    Primero edito polls/views.py para hacer que la vista vote() remita a la página de resultados de la pregunta, luego cree la plantilla polls/results.html y por último modifiqué polls/urls.py

--- tutorial_django/polls/views.py
+++ tutorial_django/polls/views.py
@@ -15,8 +15,8 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -35,5 +35,8 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+
+
 
 # Create your views here.
